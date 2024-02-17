f8811eca5a4466cc0d9435bfa7af274c934df048
Fiol Thiago <fiolthiago9@gmail.com>
2024-02-17 13:15:51 -0300

    mostramos el clima, el lugar, y la descripcion del clima del lugar

--- src/components/Resultados.jsx
+++ src/components/Resultados.jsx
@@ -1,4 +1,19 @@
+import React, { useState, useEffect } from "react";
+
 const Resultados = ({ clima, noEncontro }) => {
+    const [gradosCelcius, setGradosCelcius] = useState("");
+
+    useEffect(() => {
+        if (clima && clima.main && clima.main.temp) {
+            const celcius = convertirAKelvinACelsius(clima.main.temp);
+            setGradosCelcius(celcius + ' °C');
+        }
+    }, [clima]);
+
+    const convertirAKelvinACelsius = (kelvin) => {
+        return (kelvin - 273.15).toFixed(2);
+    };
+
     const mostrarComponente = noEncontro ? (
         <div className="my-3 Formulario py-3 px-2">
             <p className="text-center mb-0">No se encontraron resultados</p>
@@ -6,7 +21,10 @@ const Resultados = ({ clima, noEncontro }) => {
     ) : (
         <div className="my-3 Formulario py-3 px-2">
             <h3>{clima.name}</h3>
-            <p></p>
+            <p className="text-end">Temperatura: {gradosCelcius}</p>
+            {clima.weather && clima.weather.length > 0 && (
+                <p className="text-end">Descripción del clima: {clima.weather[0].description}</p>
+            )}
         </div>
     );
 
